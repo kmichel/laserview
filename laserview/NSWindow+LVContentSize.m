a3ce49d8db7050e25fb74bce365dbb6facbce80e
Kevin Michel <kmichel.info@gmail.com>
2014-10-28 17:28:58 +0100

    Fix resizing behavior

--- laserview/NSWindow+LVContentSize.m
+++ laserview/NSWindow+LVContentSize.m
@@ -5,15 +5,51 @@
 - (void)setContentSizeAnimated:(NSSize)size
 {
 	if ((self.styleMask & NSFullScreenWindowMask) == 0) {
+		NSRect const screenFrame = self.screen.visibleFrame;
+		NSRect const screenContent = [self contentRectForFrameRect:screenFrame];
+		// First ensure our window will not be larger than the available space on screen.
+		// The OS will enforce this but we must take it in account for our alignment logic.
+		if (size.width > screenContent.size.width || size.height > screenContent.size.height) {
+			CGFloat const contentRatio = size.width / size.height;
+			CGFloat const screenContentRatio = screenContent.size.width / screenContent.size.height;
+			if (contentRatio > screenContentRatio) {
+				size.width = screenContent.size.width;
+				size.height = size.width / contentRatio;
+			} else {
+				size.height = screenContent.size.height;
+				size.width = size.height * contentRatio;
+			}
+		}
+		// Then prepare and align our new frame.
+		// We stay horizontally centered but keep the title bar at the same previous height.
 		NSRect contentRect = [self contentRectForFrameRect:self.frame];
-		double const dx = contentRect.size.width - size.width;
-		double const dy = contentRect.size.height - size.height;
+		CGFloat const dx = contentRect.size.width - size.width;
+		CGFloat const dy = contentRect.size.height - size.height;
 		contentRect.origin.x += dx * 0.5f;
 		contentRect.origin.y += dy;
 		contentRect.size = size;
-		NSRect const frame = [self frameRectForContentRect:contentRect];
-		[self setFrame:frame display:YES animate:YES];
+		NSRect frame = [self frameRectForContentRect:contentRect];
+		// Then we ensure our window stays entirely inside the screen.
+		// Beware, screen frames have non-zero origins in multi-screen environments.
+		CGFloat const screenRight = screenFrame.origin.x + screenFrame.size.width;
+		CGFloat const leftOffset = frame.origin.x - screenFrame.origin.x;
+		CGFloat const rightOffset = screenRight - (frame.origin.x + frame.size.width);
+		if (leftOffset < 0 && leftOffset + rightOffset >= 0)
+			frame.origin.x = screenFrame.origin.x;
+		if (rightOffset < 0 && rightOffset + leftOffset >= 0)
+			frame.origin.x = screenRight- frame.size.width;
+		CGFloat const screenTop = screenFrame.origin.y + screenFrame.size.height;
+		CGFloat const bottomOffset = frame.origin.y - screenFrame.origin.y;
+		CGFloat const topOffset = screenTop - (frame.origin.y + frame.size.height);
+		if (bottomOffset < 0 && topOffset + bottomOffset >= 0)
+			frame.origin.y = screenFrame.origin.y;
+		if (topOffset < 0 && bottomOffset + topOffset >= 0)
+			frame.origin.y = screenTop - frame.size.height;
+		// And now we can commit our result.
+		// ContentAspectRatio must be sent before the new frame since we don't want
+		// the new frame size to be constrained by the old contentAspectRatio.
 		self.contentAspectRatio = size;
+		[self setFrame:frame display:YES animate:YES];
 	}
 }
 
